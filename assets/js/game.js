299e103ac9f86b3e0e5e25a33c993bf859ca4797
unknown <walmirlopesti@gmail.com>
2021-09-22 10:06:37 -0300

    adição de tamanhos aleatórios e posições randomicas para dinamizar o app web mata mosquito

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,8 +11,43 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
-var posicaoX = Math.floor(Math.random() * largura)
-var posicaoY = Math.floor(Math.random() * altura)
+function posicaoRandomica(){
 
-console.log(posicaoX, posicaoY)
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    //criando elemento Html
+
+    var mosca = document.createElement('img')
+    mosca.src = 'assets/imagens/mosca.png'
+    mosca.className = tamanhoAleatorio()
+    mosca.style.left = posicaoX + 'px'
+    mosca.style.top = posicaoY + 'px'
+    mosca.style.position = 'absolute'
+
+    document.body.appendChild(mosca)
+
+    tamanhoAleatorio()
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+
+        case 0:
+            return 'mosquito'
+        
+        case 1: 
+            return 'mosquito1'
+        
+        case 2: 
+            return 'mosquito2'
+
+    }
+}
